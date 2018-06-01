b6b7b281d052ce869be79d323dbfd9b846f02010
Hugh A <harcenea@gmail.com>
2018-06-01 13:56:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
    if (string === "I love you, Grandma.") return "I love you, too.";
   
   else if ( string.toLowerCase === 
-string ) return "I can't hear you!";
+string ) return "I can't hear you!" ;
 
   else if (string === string.toUpperCase()) {return "YES INDEED!";}
 }
