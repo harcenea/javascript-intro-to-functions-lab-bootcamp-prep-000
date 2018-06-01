e3c019177422958a42379cd195d03834b6f8da4d
Hugh A <harcenea@gmail.com>
2018-06-01 13:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function logWhisper(string) { console.log
 function sayHiToGrandma(string) { 
    if (string === "I love you, Grandma.") return "I love you, too.";
   
-  else if ( string.toLowerCase === 
+  else if ( whisper(string) === 
 string ) return "I can't hear you!" ;
 
-  else if (string === string.toUpperCase()) {return "YES INDEED!";}
+  else if (string === shout(string)) {return "YES INDEED!";}
 }
 
 function sayHiToGrandma(string) { if (string === string.toUpperCase()) {return "YES INDEED!"}}
